0a7edcce4fb940118ff13d2cc2947be2e11c3850
Nikhilreddy447 <128344065+Nikhilreddy447@users.noreply.github.com>
2024-09-04 18:14:12 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -50,7 +50,7 @@ function Projects() {
           <Row>
             <Col size={12}>
                   <h2>Projects</h2>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>This are the set of projects I worked on.</p>
                   <Tab.Container id="projects-tabs" defaultActiveKey="first">
                     <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                       <Nav.Item>
